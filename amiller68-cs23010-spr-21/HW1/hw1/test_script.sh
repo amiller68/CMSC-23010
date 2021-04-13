0cc7af68590065723f58677c0ed20d9d52ab6996
Alex <alexanderscottmiller@gmail.com>
2021-04-12 21:10:18 -0500

    updated test script

--- amiller68-cs23010-spr-21/HW1/hw1/test_script.sh
+++ amiller68-cs23010-spr-21/HW1/hw1/test_script.sh
@@ -9,13 +9,14 @@ for n in $N; do
     file_name="tests/${n}.txt"
     printf 'Running serial program : n = %s\n' "$n"
     ./fw_serial $file_name 1
-    s_res_file='find res/ -name "$n:1_fw_serial_*.txt"'
+    s_res_file='find res/ -name "$n:1_*"'
     for t in $T; do
         printf 'Running parallel program : n = %s | t = %s\n' "$n" "$t"
         ./fw_parallel $file_name $t
-        p_res_file='find res/ -name "$n:$t_fw_parallel_*.txt"'
+        p_res_file='find res/ -name "$n:$t_*"'
         if cmp -s "$s_res_file" "$p_res_file"; then
             printf 'SERR: Failed operation <%s:%s>\n' "$n" "$t"
+            rm "$p_res_file"
         fi
     done
 done
