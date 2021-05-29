e94a77cacdeae30e8518bbeeffdaa2efb56bff90
Alex <alexanderscottmiller@gmail.com>
2021-05-29 14:48:33 -0500

    Final data format, analysis tools, and writeup

--- amiller68-cs23010-spr-21/HW3b/hw3b/analyze.py
+++ amiller68-cs23010-spr-21/HW3b/hw3b/analyze.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from statistics import variance
+from scipy import stats
+
 Path("../Docs/graphs").mkdir(parents=True, exist_ok=True)
 
 #Overhead Experiment
@@ -120,20 +123,128 @@ for u in U:
 
                 #Plot data for Lock-Free Speedup
                 L_speedup = np.array((L_data[(L_data['W'] == w) & (L_data['U'] == u)])["Speedup"].values.tolist())
-                ax.plot(N, L_speedup, 'tab:blue', label = "Straetgy = Lock-Free, Lock = N/A")
-                for l in L:
-                    #Set The lock type label
-                    if l == 'p':
-                        lock_type = "Mutex"
-                        color = 'tab:green'
-                    else:
-                        lock_type = "Anderson"
-                        color = 'tab:red'
 
-                    #Get the comparison speedup data for
-                    Comp_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == l)])["Speedup"].values.tolist())
-                    ax.plot(N, Comp_speedup, color, label = "Straetgy = " + strategy + ", Lock = " + lock_type)
-        #Done With plotting, save an move onto the next expierment
+                p_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == 'p')])["Speedup"].values.tolist())
+                a_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == 'a')])["Speedup"].values.tolist())
+
+                ax.plot(N, L_speedup, 'tab:blue', label = "Straetgy = Lock-Free, Lock = N/A")
+                ax.plot(N, p_speedup, color = 'tab:green', label = "Straetgy = " + strategy + ", Lock = Mutex")
+                ax.plot(N, a_speedup, color = 'tab:red', label = "Straetgy = " + strategy + ", Lock = Anderson")
+
+                #Label Variance data
+                for (n,l,p,a) in zip(N,L_speedup, p_speedup, a_speedup):
+                    if s == 'H':
+                        var = variance([l,p,a])
+                    else:
+                        var = variance([p,a])
+
+                    best = max(l, p, a)
+                    if(p > a):
+                        best_l = 'p'
+                    else:
+                        best_l = 'a'
+
+                    if (l == best):
+                        label = ("{:.5f},l," + best_l).format(var)
+                    if (p == best):
+                        label = ("{:.5f},p," + best_l).format(var)
+                    if (a == best):
+                        label = ("{:.5f},a," + best_l).format(var)
+
+
+                    ax.annotate(label,(n,l), textcoords="offset points",  xytext=(0,10), ha='center')
+
+        #Iterate through subplots
+        N_ind = [2, 3, 4, 8, 14, 28]
+        workers = [1, 2, 3, 7, 13, 27]
+        if (s == 'H'):
+            print("LockFree local maximums, " + packet_method + " load:")
+            for w in W:
+                #Plot data for Lock-Free Speedup
+                L_speedup = (L_data[(L_data['W'] == w) & (L_data['U'] == u)])["Speedup"].values.tolist()
+                #Print LockFree local maximums
+                max_speedup = max(L_speedup)
+                index = L_speedup.index(max_speedup)
+                n = N_ind[index]
+                label = "$W = " + str(w) + "$: " + str(max_speedup) + "x speedup at $n = " + str(n) + "$"
+                print(label)
+
+            print("LockFree performance drops N = 14 to N = 28, " + packet_method + " load:")
+            for w in W:
+                #Plot data for Lock-Free Speedup
+                L_speedup = ((L_data[(L_data['W'] == w) & (L_data['U'] == u)])["Speedup"].values.tolist())
+                #Print LockFree local maximums
+                speedup_14 = L_speedup[N_ind.index(14)]
+                speedup_28 = L_speedup[N_ind.index(28)]
+
+                dec = ((speedup_14 - speedup_28) / speedup_14) * 100
+                label = "$W = " + str(w) + "$: " + "{:.5f}".format(dec) + "\% drop in performance"
+                print(label)
+
+            L_speedup = ((L_data[(L_data['W'] == 8000) & (L_data['U'] == u)])["Speedup"].values.tolist())
+
+
+            slope, intercept, r_value, p_value, std_err = stats.linregress(workers[0:4],L_speedup[0:4])
+
+            print("W = 8000, Lin-Reg results: slope = " + str(slope) + "  r^2 = " + str(r_value ** 2))
+
+            print("LockFree ideal scale performance, " + packet_method + " load:")
+            for n in N_ind:
+                l = L_speedup[N_ind.index(n)]
+                shy = (((n - 1) - l) / (n - 1)) * 100
+                label = "$n = " +str(n) + "$, " + str(n - 1) + " worker threads: " + "{:.5f}".format(l) + "x speedup "+ "{:.5f}".format(shy) + "\% shy of ideal scaling"
+                print(label)
+
+        else:
+            print("Awesome local maximums, averaged between lock types, " + packet_method + " load:")
+            for w in W:
+                p_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == 'p')])["Speedup"].values.tolist())
+                a_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == 'a')])["Speedup"].values.tolist())
+
+                test_speedup = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                i = 0
+                for p,a in zip(p_speedup, a_speedup):
+                    avg = (p + a) / 2
+                    test_speedup[i] = avg
+                    i = i + 1
+
+                #Print LockFree local maximums
+                max_speedup = max(test_speedup)
+                index = test_speedup.index(max_speedup)
+                n = N_ind[index]
+                label = "$W = " + str(w) + "$: " + str(max_speedup) + "x speedup at $n = " + str(n) + "$"
+                print(label)
+
+            print("Awesome performance drops N = 14 to N = 28, " + packet_method + " load:")
+            for w in W:
+                p_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == 'p')])["Speedup"].values.tolist())
+                a_speedup = np.array((Comp_data[(Comp_data['W'] == w) & (Comp_data['U'] == u) & (Comp_data['L'] == 'a')])["Speedup"].values.tolist())
+
+                test_speedup = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                i = 0
+                for p,a in zip(p_speedup, a_speedup):
+                    avg = (p + a) / 2
+                    test_speedup[i] = avg
+                    i = i + 1
+                #Print LockFree local maximums
+                speedup_14 = test_speedup[N_ind.index(14)]
+                speedup_28 = test_speedup[N_ind.index(28)]
+
+                dec = ((speedup_14 - speedup_28) / speedup_14) * 100
+                label = "$W = " + str(w) + "$: " + "{:.5f}".format(dec) + "\% drop in performance"
+                print(label)
+
+
+            slope, intercept, r_value, p_value, std_err = stats.linregress(workers[0:4],test_speedup[0:4])
+
+            print("W = 8000, Lin-Reg results: slope = " + str(slope) + "  r^2 = " + str(r_value ** 2))
+
+            print("Awesome ideal scale performance, " + packet_method + " load:")
+            for n in N_ind:
+                l = test_speedup[N_ind.index(n)]
+                shy = (((n - 1) - l) / (n - 1)) * 100
+                label = "$n = " +str(n) + "$, " + str(n - 1) + " worker threads: " + "{:.5f}".format(l) + "x speedup "+ "{:.5f}".format(shy) + "\% shy of ideal scaling"
+                print(label)
 
         handles, labels = ax.get_legend_handles_labels()
         fig.legend(handles, labels, loc='lower right')
